4905d629f59ef341fa1c23ad0a3f0609a1dcc157
ISAAR2025 <it001@isaar.in>
2025-07-11 15:56:58 +0530

    Update db.js

--- isar-backend/config/db.js
+++ isar-backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/isar_db', {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://hins37299:McOC7Ei4DrBwWG7D@isar.j1ohrbc.mongodb.net/isar_db', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
